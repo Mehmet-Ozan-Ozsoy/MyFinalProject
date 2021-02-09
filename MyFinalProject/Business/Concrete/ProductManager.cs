ad1776221e7c833c05df34290f1d63c75cc4f1a5
Mehmet-Ozan-Ozsoy <62773795+Mehmet-Ozan-Ozsoy@users.noreply.github.com>
2021-02-09 17:58:39 +0300

    Linq (foreach tipi kodların tek satırda yazılmasına olanak sağlayan kütüphane) Class Library (.NET Standart) (Aynı grup uygulamaların bir arada tutulduğu, yeni alt projeler eklerken tercih edeceğimiz proje ortamı) Empty Solution (İlk kez oluşturulacak ana proje için create new project'ten önce oluşturulacak) ConsoleUI (Program.cs'ı içeren, kulanıcının işlemlerini gerçekleştireceği arayüz ortamı) ConsoleUI hariç diğer katmanlarda Abstract (Soyut) ve Concrete (Somut) klasörlerinin oluşturulması Add/Create Reference (içeriğinden yararlanılacak alt projelerle oluşturulan bağlantı) "_" global yazım (örneğin _products; alt satırda, interface'den implemente edilen metotlarda kullanılmak üzere)

---
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
